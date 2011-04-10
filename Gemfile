f2cca47fa0308c4466bac6d2e94b9dc3141b0636
Mark Turner <markjturner@gmail.com>
2011-04-10 17:48:55 +0100

    Switching to nokogiri for parsing

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source 'http://rubygems.org/'
 gem 'sinatra', :git => 'git://github.com/sinatra/sinatra.git'
 
 gem 'bundler'
-gem 'hpricot'
+gem 'nokogiri'
 gem 'json'
--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'bundler'
 Bundler.setup
 
 require 'sinatra'
-require 'hpricot'
+require 'nokogiri'
 require 'open-uri'
 require 'json'
 
@@ -23,18 +23,18 @@ get '/' do
   
   array = []
   
-  # use Hpricot to parse the feed
-  doc = Hpricot.parse(@@feed)
+  # use Nokogiri to parse the feed
+  doc = Nokogiri::XML(@@feed)
   
   # examine first 5 items (0..4)
-  (doc/:item)[0..4].each do |item|
+  doc.search("item")[0..4].each do |item|
     
     # push items to array
     array << {
-      :title => item.search("/title").inner_html,
+      :title => item.search("title").inner_html,
       # had to use the rdf:about attribute for link because hpricot thinks link is a malformed tag!
-      :url => item.attributes['rdf:about'],
-      :description => item.search("/description").inner_text
+      :url => item.attribute("about").inner_html,
+      :description => item.search("description").inner_text
     }
   end
   
